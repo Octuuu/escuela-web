269e9f1c744d7c66b50718edcfdc9af21d69dc69
Octuuu <125304301+Octuuu@users.noreply.github.com>
2025-06-10 19:45:30 -0300

    ..

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';  
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://ifdsanignacioguazu.com',  
+  integrations: [
+    react(),
+    sitemap(),                            
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
 });
-
